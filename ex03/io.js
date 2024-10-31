0958c76b4c9e3804fd6ba32941fc985d006b1ba6
WakamiyaYuma <root1@WakamiyaYumanoMacBook-Pro.local>
2024-10-31 19:29:14 +0900

    initial commit2

--- ex03/io.js
+++ ex03/io.js
@@ -6,12 +6,18 @@
 /*   By: ywakamiy <ywakamiy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 13:46:29 by ywakamiy          #+#    #+#             */
-/*   Updated: 2024/10/31 13:46:30 by ywakamiy         ###   ########.fr       */
+/*   Updated: 2024/10/31 19:22:11 by ywakamiy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 const fs = require("fs");
-const count =
-  fs.readFileSync(process.argv[2]).toString().split("\n").length - 1;
+const fileContent = fs.readFileSync(process.argv[2]).toString();
+
+let count = 0;
+for (let i = 0; i < fileContent.length; i++) {
+  if (fileContent[i] === "\n") {
+    count++;
+  }
+}
 
 console.log(count);
--- ex04/asyncio.js
+++ ex04/asyncio.js
@@ -6,13 +6,13 @@
 /*   By: ywakamiy <ywakamiy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 13:46:31 by ywakamiy          #+#    #+#             */
-/*   Updated: 2024/10/31 13:46:32 by ywakamiy         ###   ########.fr       */
+/*   Updated: 2024/10/31 19:28:59 by ywakamiy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 const fs = require("fs");
 
 fs.readFile(process.argv[2], (err, data) => {
-  if (err) return console.error(err);
+  if (err) return console.log(err);
   console.log(data.toString().split("\n").length - 1);
 });

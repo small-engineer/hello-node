4b18907347c457bbf7135bc444ba28344137fa67
WakamiyaYuma <root1@WakamiyaYumanoMacBook-Pro.local>
2024-11-01 16:10:38 +0900

    fix h

--- ex08/time-server.js
+++ ex08/time-server.js
@@ -6,44 +6,37 @@
 /*   By: ywakamiy <ywakamiy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 13:46:43 by ywakamiy          #+#    #+#             */
-/*   Updated: 2024/11/01 16:04:05 by ywakamiy         ###   ########.fr       */
+/*   Updated: 2024/11/01 16:10:08 by ywakamiy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-const http = require("http");
+const zeroFill = (num) => {
+  return (num < 10 ? "0" : "") + num;
+};
 
-if (process.argv.length < 3) {
-  console.log("No arguments");
-  process.exit(-1);
-}
+const getCurrentTime = () => {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = zeroFill(date.getMonth() + 1);
+  const day = zeroFill(date.getDate());
+  const hours = zeroFill(date.getHours());
+  const minutes = zeroFill(date.getMinutes());
+  return `${year}-${month}-${day} ${hours}:${minutes}`;
+};
 
-const url = `http://localhost:${process.argv[2]}`;
-const timeoutDuration = 5000;
-
-try {
-  const req = http.get(url, (res) => {
-    let rawData = "";
-
-    res.setEncoding("utf8");
-
-    res.on("data", (chunk) => {
-      rawData += chunk;
-    });
-
-    res.on("end", () => {
-      console.log(rawData.length);
-      console.log(rawData);
-    });
+const net = require("net");
+const server = net.createServer((socket) => {
+  socket.on("data", (data) => {
+    const request = data.toString();
+    if (request.startsWith("GET")) {
+      const currentTime = getCurrentTime();
+      const httpResponse = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${currentTime.length}\r\n\r\n${currentTime}\n`;
+      socket.write(httpResponse);
+      socket.end();
+    }
   });
+});
 
-  req.setTimeout(timeoutDuration, () => {
-    console.log("request timed out");
-    req.abort();
-  });
-
-  req.on("error", (err) => {
-    console.log(err.message);
-  });
-} catch (err) {
-  console.log(err.message);
-}
+server.listen(process.argv[2], () => {
+  console.log(`Server listening on port ${process.argv[2]}`);
+});
